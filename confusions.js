0d0b60244977403dfc927c1c83a3921cf34d6e21
AliasT <AliasT@users.noreply.github.com>
2016-04-28 16:11:08 +0800

    Update confusions.js

--- confusions.js
+++ confusions.js
@@ -16,10 +16,13 @@ $element.scrollTop()
 $element.scrollLeft()
 
 // 元素顶部到窗口顶部的距离(父级元素没有滚动的情况下,待我有时间再算)
-eTop$(window).scrollTop() - $element.offset().top
+elemtop_to_wintop = $(window).scrollTop() - $element.offset().top;
 
-// 元素顶部到窗口底部的距离(加上元素高度即可)(同上)
-$(window).scrollTop() - $element.offset().top - $element.height()
+// 元素顶部到窗口底部的距离
+elembottom_to_winbottom = $(window).height() - elem_top_windowtop 
 
 // 元素底部到窗口顶部的距离(加上元素高度即可)(同上)
-$(window).scrollTop() - $element.offset().top + $element.height()
+elembottom_to_wintop = elemtop_to_wintop + $element.height()
+
+// 元素底部到窗口底部的距离
+elembottom_to_winbottom = $(window) - elembottom_to_wintop
